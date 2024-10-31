0068a29e1258ff37137fe04c5d822465bd86db55
Jason-Hargrove <rijaha@gmail.com>
2024-10-30 23:06:25 -0500

    Added image to locations

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -2,6 +2,8 @@ export type Location = {
   id: number;
   name: string;
   position: { lat: number; lng: number };
+  description?: string;
+  image?: string;
 };
 
 export const locations: Location[] = [
@@ -14,6 +16,8 @@ export const locations: Location[] = [
       id: 2,
       name: "Frist Art Museum",
       position: { lat: 36.1527, lng: -86.7836 },
+      description: "The Frist Art Museum is a contemporary art museum in Nashville, Tennessee, that is known for its collection of modern and contemporary art.",
+      image: "/images/frist-art-museum.jpg"
     },
     {
       id: 3,
