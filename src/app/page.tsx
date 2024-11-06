ffb8f18c27913e79b2fc0d5a22d110f9b1648ed6
Jason-Hargrove <rijaha@gmail.com>
2024-11-05 21:33:26 -0600

    Adding images for hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 const Home: FC = () => {
   return (
     <div className="p-4">
-      <section className="text-center mb-8">
+      <section className="relative flex items-center justify-center h-[400px] text-white">
+        <picture className=""></picture>
         <h1 className="text-4xl font-bold mb-4">Welcome to Art Explorer</h1>
         <p className="text-lg mb-6">
           Discover and explore art locations around the world. From renowned museums to hidden galleries, Art Explorer connects you to a world of culture and creativity.
